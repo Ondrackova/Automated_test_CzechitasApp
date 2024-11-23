58d1ba246fdf196c71523658d7e620b8711d97c2
Zuzana Ondrackova <ondrac.zuzana@gmail.com>
2024-11-23 20:01:57 +0100

    Changes in tests - new random names, still can by colaps with output "zadne zaznamy nebyly nalezeny" and cencelled application. Test clearApplicationList not working good, because it only cancelled application, not deleded it.

--- CzechitasTest/src/main/java/RandomNameGenerator.java
+++ CzechitasTest/src/main/java/RandomNameGenerator.java
@@ -4,7 +4,7 @@ public class RandomNameGenerator {
 
         // function for generating random surname
         public static String randomNameGenerator() {
-            String[] surnames = {"Smith", "Johnson", "Williams", "Brown", "Jones"};
+            String[] surnames = {"Novak", "Stastny", "Novotny", "Hrubes", "Sova" , "Hruda" , "Bobes" , "Luza" , "Peroutka" , "Chaloupka" , "Sovak" , "Brabec" , "Hrubec" , "Gudas" , "Pasternak" , "Zilka" , "Janak" , "Kanonyr" , "Jednicka" , "Vomacka" , "Opustil", "Veverka"};
             Random random = new Random();
             int randomIndex = random.nextInt(surnames.length);  // random index
             return surnames[randomIndex];
---
+++ CzechitasTest/src/main/java/czechitas/cz/ExpectedOutput.java
@@ -0,0 +1,28 @@
+package czechitas.cz;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.regex.Pattern;
+
+public class ExpectedOutput {
+
+    WebDriver browser;
+    WebDriverWait browserWait;
+
+    //browser inicialization
+    public ExpectedOutput(WebDriver browser) {
+        this.browser = browser;
+        this.browserWait = new WebDriverWait(browser, Duration.ofSeconds(5));
+    }
+
+    // Method to check if the output matches expected values
+    public static boolean isExpectedOutput(String emptyApplication) {
+
+        return emptyApplication.equals("Žádné záznamy nebyly nalezeny");
+
+    }
+
+}
